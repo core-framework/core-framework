9042e9dd1e94c3659b8d901b9c5838d9149cafb6
Shalom Sam <shalom.s@directi.com>
2015-01-24 18:57:58 +0530

    Modified metas structure in conf file, added  index to conf, enabled reading metas & titles from file defined in routes conf

---
+++ demoapp/metas/metas.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This is just an example implementation of metas and titles
+ */
+
+return [
+
+    '/documentation/routing' => [
+        'pageTitle' => 'CoreFramework - Learning about Routing in web application using CoreFramework',
+        'keywords' => 'some example',
+        'description' => 'Learning routing'
+    ]
+
+];
