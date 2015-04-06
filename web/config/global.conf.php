46e530b91ad308a8fe53ae12e25177cf66bdd16a
Shalom Sam <shalom.s@directi.com>
2015-04-07 01:41:34 +0530

    adding mistakenly removed base conf files

---
+++ web/config/global.conf.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the Core Framework package.
+ *
+ * (c) Shalom Sam <shalom.s@coreframework.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+//global $_CONFIG;
+
+$global = [
+
+    '$global' => [
+        'metaAndTitleFromFile' => false,
+        'appPath' => realpath(__DIR__ . DIRECTORY_SEPARATOR . '..'),
+        'basePath' => realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..'),
+        'apcIsLoaded' => extension_loaded('apc'),
+        'apcIsEnabled' => ini_get('apc.enabled'),
+        'tplType' => 'tpl',
+        'useAPC' => true,
+        'useAestheticRouting' => false
+    ],
+    '$db' => require('db.conf.php'),
+    '$routes' => require('routes.conf.php')
+
+];
+
+if ($global['$global']['apcIsLoaded'] && $global['$global']['apcIsEnabled']) {
+    $global['$global']['hasAPC'] = true;
+} else {
+    $global['$global']['hasAPC'] = false;
+}
+
+return $global;
